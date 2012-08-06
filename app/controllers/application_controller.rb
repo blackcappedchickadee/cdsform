d1b93fc90d8a27e385f09ec8ce6110ddfd932f5f
Cloudchirp <cloudchirp.chirpmaster@gmail.com>
2012-08-05 23:25:37 -0400

    Bugfixes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,6 +10,7 @@ class ApplicationController < ActionController::Base
   def restrict_access
     whitelisted_ipaddress = Cdsform::Application.config.restrictedip
     whitelist = [whitelisted_ipaddress].freeze
+    puts "request.remote_ip = #{request.remote_ip}"
     unless whitelist.include? request.remote_ip
       redirect_to access_denied_path
     end
