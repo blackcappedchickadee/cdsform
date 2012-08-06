f2ffcd3e63dc1517aa87473a9a09fd1afdcef4c5
Cloudchirp <cloudchirp.chirpmaster@gmail.com>
2012-08-05 23:32:45 -0400

    Bugfixes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,20 +1,4 @@
 class ApplicationController < ActionController::Base
-  #protect_from_forgery
-  
-  before_filter :restrict_access
-  
-  def process_form
-  end
-  
-  private 
-  def restrict_access
-    whitelisted_ipaddress = Cdsform::Application.config.restrictedip
-    whitelist = [whitelisted_ipaddress].freeze
-    puts "request.remote_ip = #{request.remote_ip}"
-    unless whitelist.include? request.remote_ip
-      redirect_to access_denied_path
-    end
-    
-  end
+  protect_from_forgery
   
 end
