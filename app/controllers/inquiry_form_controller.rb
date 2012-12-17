ffeba625870f7d7b14edd4e66f9f61e62d8297d5
Cloudchirp <cloudchirp.chirpmaster@gmail.com>
2012-12-16 20:25:10 -0500

    Changing form post logic a bit.

--- app/controllers/inquiry_form_controller.rb
+++ app/controllers/inquiry_form_controller.rb
@@ -12,24 +12,27 @@ class InquiryFormController < ApplicationController
     recaptch_url = Cdsform::Application.config.recaptchaurl
     challenge = params[:recaptcha_challenge_field]
     response = params[:recaptcha_response_field]
-    #puts "configs --- #{recaptch_public}, #{recaptch_private}, #{remote_ip_addr}, #{recaptch_url}"
+    puts "configs --- #{recaptch_public}, #{recaptch_private}, #{remote_ip_addr}, #{recaptch_url}"
     
     name = params[:contactname]
-    phone = params[:phonenumber]
     email = params[:email]
+    phone = params[:phonenumber]
     performance_dates = params[:performancedate]
     locations = params[:performancelocation]
     budget = params[:budget]
     additional_comments = params[:comments]
-    event_type = "#{params[:radios]} #{params[:otherdesc]}"
+    event_type = params[:perftype]   #   "#{params[:radios]} #{params[:otherdesc]}"
     redirect_after_save = params[:redirecturl]
     backurl_failure_recaptcha = params[:backurl]
+    backurl_failure_other = params[:backurlother]
 
     params = {'privatekey' => recaptch_private, 'remoteip' => remote_ip_addr,
               'challenge' => challenge, 'response' => response}
               
     x = Net::HTTP.post_form(URI.parse(recaptch_url), params)
     retval = x.body
+    
+    puts "recaptcha result #{retval}..."
     
     #REMOVE when done!
     #retval = "true"
@@ -45,6 +48,8 @@ class InquiryFormController < ApplicationController
     else
       if retval.include?("incorrect-captcha-sol")
         redirect_to backurl_failure_recaptcha
+      else
+        redirect_to backurl_failure_other
       end
     end
     
